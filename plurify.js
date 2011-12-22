6e67c608479d64d16d9528726590a25604cfff07
krabicezpapundeklu <krabicezpapundeklu@gmail.com>
2011-12-22 11:09:49 +0100

    Add simple support for simple expressions.

--- plurify.js
+++ plurify.js
@@ -1,7 +1,34 @@
 (function(window) {
+	function parseExpression(input, position, parameters, builder) {
+		var closeBracket = input.indexOf("}", position);
+		builder.push(parameters[input.slice(position + 1, closeBracket)]);
+		return closeBracket + 1;
+	}
 
-	window.plurify = function(input) {
-		return input || "";
+	window.plurify = function(input, parameters) {
+		if(input) {
+			var position = 0;
+			var openBracket = input.indexOf("{", position);
+
+			if(openBracket === -1) {
+				return input;
+			}
+
+			var builder = [];
+
+			do {
+				builder.push(input.slice(position, openBracket));
+				position = parseExpression(input, openBracket, parameters, builder);
+				openBracket = input.indexOf("{", position);
+			}
+			while(openBracket !== -1);
+
+			builder.push(input.slice(position));
+
+			return builder.join("");
+		}
+
+		return "";
 	}
 
 })(window);
